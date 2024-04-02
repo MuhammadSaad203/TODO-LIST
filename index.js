4f0478d4bee87904c98bc54c3358b3dc0987b887
Muhammad Saad <160244669+MuhammadSaad203@users.noreply.github.com>
2024-04-03 03:53:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let condition = true;
+console.log(chalk.blue.bold("\n\t WELCOME TO YOUR TODO LIST, What is in your mind today? \n "));
+while (condition) {
+    let task = await inquirer.prompt([
+        {
+            name: "Task",
+            type: "input",
+            message: chalk.green.bold("What is your new task?"),
+        },
+    ]);
+    todoList.push(task.Task);
+    console.log(chalk.gray("\n Your task is to :", task.Task));
+    let moreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.red.bold("Would you like to add another task?"),
+            default: "False",
+        },
+    ]);
+    condition = moreTask.addmore;
+}
+console.log(chalk.yellow.bold("\n This is your Todos : "));
+console.log(todoList);
